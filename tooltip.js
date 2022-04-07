63271e2fa96ded7f0a8fe7c9c3885d25436fa5d3
Liidia Laada <liidia.laada@gmail.com>
2022-04-07 09:03:24 +0300

    First commit

---
+++ tooltip.js
@@ -0,0 +1,10 @@
+class Tooltip extends HTMLElement {
+    constructor() {
+        super();
+        const tooltipIcon = document.createElement('span');
+        tooltipIcon.textContent = ' (?)';
+        this.appendChild(tooltipIcon);
+    }
+}
+
+customElements.define('ll-tooltip', Tooltip);
