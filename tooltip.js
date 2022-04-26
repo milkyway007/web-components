93eef3517275d97abde9d4a4a9cf8ab10495d65c
Liidia Laada <liidia.laada@gmail.com>
2022-04-26 11:07:24 +0300

    Tootip completed, confirm link.

--- tooltip.js
+++ tooltip.js
@@ -1,9 +1,42 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
-        this.appendChild(tooltipIcon);
+        this._tooltipContainer;
+        this._tooltipText = 'Some dummy tootip text.';
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                    z-index: 10;
+                }
+            </style>
+            <slot>Some default</slot>
+            <span> (?)</span>`
+        ;
+    }
+
+    connectedCallback() {
+        if(this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span');
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
+    }
+
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.shadowRoot.appendChild(this._tooltipContainer);
+    }
+
+    _hideTooltip() {
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
