ee73f93cb9a7766cd22dfd08345d8bd94b2c92d6
Liidia Laada <liidia.laada@gmail.com>
2022-05-06 12:20:56 +0300

    Styling slot, host, host context, css variables.

--- tooltip.js
+++ tooltip.js
@@ -7,14 +7,45 @@ class Tooltip extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <style>
                 div {
+                    font-weight: normal;
                     background-color: black;
                     color: white;
                     position: absolute;
+                    top: 1.5rem;
+                    left: 0.75rem;
                     z-index: 10;
+                    padding: 0.15rem;
+                    border-radius: 3px;
+                    box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
+                }
+
+                .highlight {
+                    background-color: red;
+                }
+
+                ::slotted(.highlight) {
+                    border-bottom: 1px dotted red;
+                }
+
+                .icon {
+                    background: black;
+                    color: white;
+                    padding: 0.15rem 0.5rem;
+                    text-align: center;
+                    border-radius: 50%;
+                }
+
+                :host(.important) {
+                    background: var(--color-primary, #ccc);
+                    padding: 0.15rem;
+                }
+
+                :host-context(p) {
+                    font-weight: bold;
                 }
             </style>
             <slot>Some default</slot>
-            <span> (?)</span>`
+            <span class="icon">?</span>`
         ;
     }
 
